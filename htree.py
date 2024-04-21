8fd37deb0c3f1b3872233f54919ef95bd04ec6ff
Yanzheng(Dexter) Wu <ywu1188@163.com>
2024-04-21 14:55:49 -0400

    1

--- htree.py
+++ htree.py
@@ -5,26 +5,28 @@ import matplotlib.pyplot as plt
 def htree_built(data):
     print("start building graph...")
     G = nx.DiGraph()
-    level = 0
-    limit = len(data)
+
+    root_node = "ROOT"
+    G.add_node(root_node)
+
     for group, node_stats in data.items():
-        G.add_node(group, layer = level)
+        G.add_node(group)
+        G.add_edge(root_node, group)
+
         for stat in node_stats:
             parsed = stat.split('NodeStats')
             node_name = f"{parsed[0].strip()[:-1]}_{group}"
             stat_per_node = parsed[1].strip("\n")  
-            G.add_node(node_name, label=stat_per_node, layer=level)
+            G.add_node(node_name, label=stat_per_node)
             G.add_edge(group, node_name)
-        level += 1
 
     print("Done.")
     return G
 
 def visual(G):
-    pos = nx.multipartite_layout(G, subset_key="layer")
-    labels = nx.get_node_attributes(G, 'label')
-    nx.draw(G, pos, with_labels=True, node_size=100, node_color='skyblue', font_size=2, font_weight='bold')
-    print("Done.")
+    pos = nx.spring_layout(G)
+    # labels = nx.get_node_attributes(G, 'label')#Dont show labels on the graph
+    nx.draw(G, pos, with_labels=True, node_size=500, node_color='skyblue', font_size=5, font_weight='bold')
     plt.show()
 
 if __name__ == "__main__":
